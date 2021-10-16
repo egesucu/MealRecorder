6de02d1f6f6bb9d419e156f9b92e61143150fd4b
Ege Sucu <egesucu@icloud.com>
2021-10-16 14:32:26 +0300

    Initial Commit

---
+++ MealRecorder/MealRecorderApp.swift
@@ -0,0 +1,20 @@
+//
+//  MealRecorderApp.swift
+//  MealRecorder
+//
+//  Created by Ege Sucu on 16.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MealRecorderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
